e7572e0d0c89e5fbeec60017e76b9d1af4f250f9
Anar0316 <anarlkahgvadolgor@gmail.com>
2021-04-05 12:17:08 +0800

    Added shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,93 @@
-import 'service_locator.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 void main() {
-  setupServiceLocator();
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: BodyWidget(),
+      ),
+    );
+  }
+}
+
+class BodyWidget extends StatefulWidget {
+  const BodyWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _BodyWidgetState createState() => _BodyWidgetState();
+}
+
+class _BodyWidgetState extends State<BodyWidget> {
+  var myFontSize = 20.0;
+  @override
+  void initState() {
+    _getFontSize();
+    super.initState();
+  }
+
+  Future<void> _getFontSize() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    myFontSize = prefs.getDouble('UserFontSize') ?? 20.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "My wonderful app.",
+          style: TextStyle(fontSize: myFontSize),
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() async {
+                  myFontSize = 10.0;
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.setDouble('UserFontSize', myFontSize);
+                });
+              },
+              child: Text('Small'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() async {
+                  myFontSize = 20.0;
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.setDouble('UserFontSize', myFontSize);
+                });
+              },
+              child: Text('Medium'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() async {
+                  myFontSize = 50.0;
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.setDouble('UserFontSize', myFontSize);
+                });
+              },
+              child: Text('Large'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
 }
